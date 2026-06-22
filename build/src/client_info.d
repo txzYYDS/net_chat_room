953982925c0b7151578ea04f3538cc04268501d2
Tangxuzhou <2629310036@qq.com>
2026-06-22 20:07:08 +0800

    删除编译中间项

--- build/src/client_info.d
+++
@@ -1,5 +0,0 @@
-build/src/client_info.o: src/client_info.c inc/client_info.h inc/list.h \
- inc/common.h
-inc/client_info.h:
-inc/list.h:
-inc/common.h:
--- build/src/list.d
+++
@@ -1,3 +0,0 @@
-build/src/list.o: src/list.c inc/list.h inc/common.h
-inc/list.h:
-inc/common.h:
